1734607ed3341c53bb264bcf33195bffa8ebd556
FatFriedFish <35781609+FatFriedFish@users.noreply.github.com>
2018-04-05 13:07:22 -0700

    Add files via upload
    Bilinear resize figures

---
+++ bilinear.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  5 10:12:23 2018
+
+@author: Yuhan.Long
+"""
+
+import cv2
+import numpy as np
+import math
+
+def bilinear_resize(img, m, n):
+    height, width, channel = img.shape
+    resize = np.zeros((m, n, channel), np.uint8);
+    
+    sh = height / m
+    sw = width / n
+    
+    for row in range(m):
+        y0 = int(row * sh)
+        y1 = y0 if y0 + 1 >= height else y0 + 1
+        u = row*sh - y0
+        for col in range(n):
+            x0 = int(col * sw)
+            x1 = x0 if x0 + 1 >= width else x0 + 1
+            v = col * sw - x0
+            
+            # coefficients
+            a, b, c, d = ((1-u)*(1-v)), ((1-u)*v), (u*(1-v)), (u*v)
+            
+            value = [0,0,0]
+            for l in range(3):
+                value[l] = int(img[y0, x0, l] * a + img[y1, x0,l] * b + img[y0, x1,l] * c + img[y1, x1, l] * d)
+            resize[row, col] = (value[0], value[1], value[2])
+    return resize
+
+#%%
+img = cv2.imread('cat.0.jpg')
+zoom = bilinear_resize(img, 400, 600)
+#%%
+cv2.imshow('resize', zoom)
+cv2.imshow('original', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+#%%
